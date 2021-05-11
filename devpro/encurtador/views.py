fa2fb55d3b70878fb559ad905ad5cf38f8963285
Renzo <renzon@gmail.com>
2021-05-11 13:50:06 -0300

    Importados arquivos estáticos e criado template da página de relatório

--- devpro/encurtador/views.py
+++ devpro/encurtador/views.py
@@ -5,6 +5,16 @@ from django.shortcuts import render, redirect
 from devpro.encurtador.models import UrlRedirect
 
 
+def relatorios(requisicao, slug):
+    url_redirect = UrlRedirect.objects.get(slug=slug)
+    url_reduzida = requisicao.build_absolute_uri(f'/{slug}')
+    contexto = {
+        'reduce': url_redirect,
+        'url_reduzida': url_reduzida,
+    }
+    return render(requisicao, 'encurtador/relatorio.html', contexto)
+
+
 def redirecionar(requisicao, slug):
     url_redirect = UrlRedirect.objects.get(slug=slug)
     return redirect(url_redirect.destino)
--- devpro/urls.py
+++ devpro/urls.py
@@ -21,4 +21,5 @@ from devpro.encurtador import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('<slug:slug>', views.redirecionar),
+    path('relatorios/<slug:slug>', views.relatorios),
 ]
